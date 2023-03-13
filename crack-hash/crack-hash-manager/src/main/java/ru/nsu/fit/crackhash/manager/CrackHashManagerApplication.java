79d05bfae336b64288a389acb07af8621c571075
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-03-13 20:49:38 +0700

    Init project

---
+++ crack-hash/crack-hash-manager/src/main/java/ru/nsu/fit/crackhash/manager/CrackHashManagerApplication.java
@@ -0,0 +1,11 @@
+package ru.nsu.fit.crackhash.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackHashManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CrackHashManagerApplication.class, args);
+	}
+}
