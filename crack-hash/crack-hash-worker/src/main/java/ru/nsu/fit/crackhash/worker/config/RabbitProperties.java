e1441cfbd1b92048e77f99d00cc85a34b5127479
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-10 18:59:08 +0700

    Add rabbitmq configs

---
+++ crack-hash/crack-hash-worker/src/main/java/ru/nsu/fit/crackhash/worker/config/RabbitProperties.java
@@ -0,0 +1,26 @@
+package ru.nsu.fit.crackhash.worker.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+public class RabbitProperties {
+    @Value("${crack-hash.rabbitmq.exchange.direct}")
+    private String directExchangeName;
+
+    @Value("${crack-hash.rabbitmq.route.manager}")
+    private String managerRoutingKey;
+
+    @Value("${crack-hash.rabbitmq.route.worker}")
+    private String workerRoutingKey;
+
+    @Value("${crack-hash.rabbitmq.queue.manager}")
+    private String managerQueueName;
+
+    @Value("${crack-hash.rabbitmq.queue.worker}")
+    private String workerQueueName;
+}
