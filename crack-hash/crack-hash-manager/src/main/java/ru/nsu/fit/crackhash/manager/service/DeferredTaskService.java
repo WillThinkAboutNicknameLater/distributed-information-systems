d72d9051e6bcfe97f5805c26212fa4af6601024b
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-10 19:14:27 +0700

    Add logs

--- crack-hash/crack-hash-manager/src/main/java/ru/nsu/fit/crackhash/manager/service/DeferredTaskService.java
+++ crack-hash/crack-hash-manager/src/main/java/ru/nsu/fit/crackhash/manager/service/DeferredTaskService.java
@@ -1,5 +1,6 @@
 package ru.nsu.fit.crackhash.manager.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.nsu.fit.crackhash.manager.mapper.TaskMapper;
 import ru.nsu.fit.crackhash.manager.queue.message.WorkerTaskRequest;
@@ -7,6 +8,7 @@ import ru.nsu.fit.crackhash.manager.queue.producer.WorkerTaskProducer;
 import ru.nsu.fit.crackhash.manager.storage.model.DeferredTask;
 import ru.nsu.fit.crackhash.manager.storage.repository.DeferredTaskRepository;
 
+@Slf4j
 @Service
 public class DeferredTaskService {
     private final DeferredTaskRepository deferredTaskRepository;
@@ -22,10 +24,14 @@ public class DeferredTaskService {
     }
 
     public void deferTask(DeferredTask task) {
+        log.info("Request #{}. Deferring part #{}", task.getRequestId(), task.getPartNumber());
+
         deferredTaskRepository.save(task);
     }
 
     public void distributeTasks() {
+        log.info("Distributing tasks");
+
         for (DeferredTask deferredTask : deferredTaskRepository.findAll()) {
             WorkerTaskRequest workerTaskRequest = taskMapper.map(deferredTask);
             workerTaskProducer.sendTask(workerTaskRequest);
