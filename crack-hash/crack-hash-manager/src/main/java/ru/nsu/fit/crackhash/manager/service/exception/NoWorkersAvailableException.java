f4ecc333678db8c2ffc3d22169cec8eabe9ff3af
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-10 18:53:57 +0700

    Remove ManagerService and exception

--- crack-hash/crack-hash-manager/src/main/java/ru/nsu/fit/crackhash/manager/service/exception/NoWorkersAvailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.crackhash.manager.service.exception;
-
-public class NoWorkersAvailableException extends RuntimeException {
-    public NoWorkersAvailableException() {
-        super("No workers available");
-    }
-}
